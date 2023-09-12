0a6f0994c1937593703181f24b0e6ceff9da4992
panuseng <94435152+panuseng@users.noreply.github.com>
2023-09-12 17:17:03 +0700

    Add files via upload

---
+++ ft_*.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/*int	isalpha(int c)
+{
+
+}*/
+
+void	ft_isupper (unsigned char a)
+{
+	if (a >= 101 && a <- 132)
+		printf("s");
+}
+
+int main(int argc, char* argv[])
+{
+	ft_isupper(argv[1]);
+}
